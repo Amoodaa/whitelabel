146ec561649bbe09757ace65321a6de14eb2febe
Imad Shatali <imad.shatali@gmail.com>
2020-09-02 19:46:39 +0300

    client works without bugs

---
+++ client/craco.config.js
@@ -0,0 +1,5 @@
+const CracoAntDesignPlugin = require("craco-antd");
+// list of modifiable vars https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  plugins: [{ plugin: CracoAntDesignPlugin }],
+};
